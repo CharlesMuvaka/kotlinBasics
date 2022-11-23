30b8822988ad94338a51f8b0bc9d0cc88be0ac13
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-11-23 08:12:12 +0300

    create a kotlin learning class

---
+++ src/main/kotlin/Student.kt
@@ -0,0 +1,6 @@
+class Student {
+    //Testing about kotlin classess concepts
+    var name: String? = null //initialized a member variable of text type. Cannot be accessed outside
+    var studentId:Int? = 0
+    var studentRegNumber: Int? = 0
+}
