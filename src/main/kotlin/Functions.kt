06dabf3b3130cdfe2b26dbbb46db375d3268caff
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-12-23 18:43:59 +0300

    create an extensive function that adds two numbers

--- src/main/kotlin/Functions.kt
+++ src/main/kotlin/Functions.kt
@@ -1,7 +1,13 @@
 fun main() {
     println(addNumbers(10,20))
+    println(20.addNumbers())
 }
 //Declaring helper functions
 fun addNumbers(number: Int, numberTwo: Int ): Int{
     return number + numberTwo
+}
+
+//Declaring extensive functions
+fun Int.addNumbers(): Int {
+    return this + 10
 }
