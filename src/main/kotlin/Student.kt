981a6d862a49890b137950993576224cf51e4d7c
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-12-27 03:36:14 +0300

    create a nested class.

--- src/main/kotlin/Student.kt
+++ src/main/kotlin/Student.kt
@@ -3,4 +3,10 @@ class Student {
     var name: String? = null //initialized a member variable of text type. Cannot be accessed outside
     var studentId:Int? = 0
     var studentRegNumber: Int? = 0
+
+    //initialise a nested class
+    class Course{
+       var courseName: String? = null
+       val courseId:Int? = 0;
+    }
 }
