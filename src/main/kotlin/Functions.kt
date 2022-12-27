e691cb19631b94cfaafeb9a73d925fc5a417feef
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-12-27 03:37:19 +0300

    lesson on extensive and helper functions in kotlin.

--- src/main/kotlin/Functions.kt
+++ src/main/kotlin/Functions.kt
@@ -1,6 +1,12 @@
 fun main() {
-    println(addNumbers(10,20))
-    println(20.addNumbers())
+//    println(addNumbers(10,20))
+//    println(20.addNumbers())
+//    println(20.addNumber(20))
+
+    val intArray = displayFinobaciNumbers(0,5)
+    for (number in intArray){
+        println(number)
+    }
 }
 //Declaring helper functions
 fun addNumbers(number: Int, numberTwo: Int ): Int{
@@ -10,4 +16,19 @@ fun addNumbers(number: Int, numberTwo: Int ): Int{
 //Declaring extensive functions
 fun Int.addNumbers(): Int {
     return this + 10
+}//Declaring extensive functions
+fun Int.addNumber(number: Int): Int {
+    return this + number
+}
+
+fun displayFinobaciNumbers(start:Int, end:Int): HashSet<Int>{
+
+    val finobaciNumbers = HashSet<Int>()
+    finobaciNumbers.add(start)
+    for (i in start..end){
+        var numberIterator: Iterator<Int> = finobaciNumbers.iterator()
+        println(numberIterator.toString())
+//        var secondNumber =  finobaciNumbers.
+    }
+    return finobaciNumbers
 }
