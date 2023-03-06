3f1c00f9868570eab43081573d333d39d8bfb089
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-06 23:53:47 +0300

    feat: looping through the matrix to get the nodes connected to the start node.

--- src/main/kotlin/datastructures/models/Graph.kt
+++ src/main/kotlin/datastructures/models/Graph.kt
@@ -15,7 +15,29 @@ class Graph(private val vertices: Int) {
         //initialising a stack to store the graph nodes to be traversed
         val stack = Stack<Int>()
 
+        //adding the starting node to the stack
+        stack.add(startNode)
 
+        //looping through the matrix to get the nodes connected to the start node
+        for (i in matrix.indices){
+            if (i == startNode){
+                while (stack.size != 0){
+                    val currentNode = stack.pop()
+                    //printing the current node to the console
+                    println(currentNode)
+                    for (j in matrix[currentNode].indices){
+                        // checking to see whether the current node has neighbors
+                        if (matrix[currentNode][j] == 1){
+                            //adding the current node's neighbor to the stack
+                            stack.push(j)
+                        }
+                    }
+                }
+
+            }else{
+                println("The node entered isn't present in the graph")
+            }
+        }
 
     }
 }
