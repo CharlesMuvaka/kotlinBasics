6e4a37eb2696b87ad864c6c68dc90f84bda34e60
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-06 23:40:46 +0300

    feat: initialise a stack to be used in the depth first traversal.

--- src/main/kotlin/datastructures/models/Graph.kt
+++ src/main/kotlin/datastructures/models/Graph.kt
@@ -1,9 +1,21 @@
 package datastructures.models
 
+import java.util.*
+
 class Graph(private val vertices: Int) {
     val matrix = Array(vertices){IntArray(vertices)}
 
+    //method to add graph edges in the matrix
     fun addGraphEdges(i: Int, j: Int, k: Int){
         matrix[i][j] = k
+    }
+
+    //method to perform a depth first traversal
+    fun depthFirstTraversal(startNode: Int){
+        //initialising a stack to store the graph nodes to be traversed
+        val stack = Stack<Int>()
+
+
+
     }
 }
