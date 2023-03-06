a027f28f1fb42ad1082537fdea6925233f12e88f
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-03-06 23:37:43 +0300

    feat: define a method to add edges to the matrix.

--- src/main/kotlin/datastructures/models/Graph.kt
+++ src/main/kotlin/datastructures/models/Graph.kt
@@ -3,5 +3,7 @@ package datastructures.models
 class Graph(private val vertices: Int) {
     val matrix = Array(vertices){IntArray(vertices)}
 
-
+    fun addGraphEdges(i: Int, j: Int, k: Int){
+        matrix[i][j] = k
+    }
 }
