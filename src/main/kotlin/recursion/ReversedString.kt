6c14fef1d2f10db69e37233cdc315d9dcef7736b
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-04-13 23:16:36 +0300

    ft: define a method to check if a string is a palindrome.

--- src/main/kotlin/recursion/ReversedString.kt
+++ src/main/kotlin/recursion/ReversedString.kt
@@ -8,10 +8,23 @@ class ReversedString {
         }
         return reverseString(input.substring(1)) + input.subSequence(0,1)
     }
-}
 
+    fun isPalindrome(input: String): Boolean{
+        //base case
+        if (input.isEmpty() || input.length == 1 ){
+            return true
+        }
+
+        //performing the check recursively.
+        if (input.substring(0,1) == input.substring(input.length - 1)){
+            return isPalindrome(input.substring(1, input.length -1))
+        }
+        return false
+    }
+}
 
 
 fun main() {
     println(ReversedString().reverseString("You are good"))
+    println(ReversedString().isPalindrome("eye"))
 }
