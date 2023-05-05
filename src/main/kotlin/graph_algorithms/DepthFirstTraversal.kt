2698277ba06ef6dacc1e2b7a391c6ddc59010504
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-05-05 19:41:13 +0300

    ft: create a matrix from the input file.

---
+++ src/main/kotlin/graph_algorithms/DepthFirstTraversal.kt
@@ -0,0 +1,41 @@
+package graph_algorithms
+
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+import java.io.IOException
+import java.util.Stack
+
+fun main() {
+    try {
+        val file = File("depthFirstTraversal.in")
+        if (file.exists()) {
+            val reader = FileReader(file)
+            val buffer = BufferedReader(reader)
+
+            val rows = buffer.readLine().toInt()
+            val columns = buffer.readLine().toInt()
+
+            val matrix = Array(rows) { IntArray(columns) }
+
+            for (i in 0 until rows) {
+                // creating a list of the matrix edges
+                val edges = buffer.readLine().split(" ")
+                for (j in edges.indices) {
+                    val edge = edges[j].toInt()
+                    matrix[i][j] = edge
+                }
+            }
+            println("------------- Iterative Depth First Traversal--------------------------")
+            depthFirstTraversal(matrix[0][0], matrix)
+            println()
+            println("------------- Recursive Depth First Traversal--------------------------")
+            recursiveDepthFirstTraversal(matrix[0][0], matrix)
+        } else {
+            file.createNewFile()
+        }
+    } catch (e: IOException) {
+        println(e.message)
+    }
+}
+
