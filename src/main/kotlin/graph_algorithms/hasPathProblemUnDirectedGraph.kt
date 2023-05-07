4fbd09ca4fb9ca2a79e87c7632fc087af8407dff
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-05-07 09:11:33 +0300

    ft: undirected graphs introduction.

--- src/main/kotlin/graph_algorithms/hasPathProblemUnDirectedGraph.kt
+++ src/main/kotlin/graph_algorithms/hasPathProblemUnDirectedGraph.kt
@@ -7,6 +7,28 @@ import java.io.File
 import java.io.FileReader
 import java.io.IOException
 
+/*
+                                UnDirected graphs
+   - These type of graphs the direction of the edges is not indicated
+                i ---- j, meaning its bidirectional.
+   - While traversing through these graphs, we might get trapped in infinite loops. Imagine
+                    i
+                   / \
+                  j---k, Below is the graph visually.
+     - val graph = arrayListOf(
+        i : [j, k],
+        j : [i, k],
+        k : [i, j]
+     )
+     - When our start node in the traversal is i, we consider its neighbors, j and k and add them to the
+        stack, we pop out the last node which will be k in our case and then consider its neighbors, i and j
+        and add them in the stack, we pop out the last node which is j and consider its neighbors, i and k
+        and add them in the stack. When we pop out our last node, it will be k which we had already traversed through.
+
+        - To counter this problem, we have to create a bag to monitor out traversed nodes. This bag will help us in
+          terminating the infinite loop.
+ */
+
 fun main(){
     //defining node names
     val nodeNames = charArrayOf('i', 'j', 'k', 'l', 'm', 'n', 'o')
