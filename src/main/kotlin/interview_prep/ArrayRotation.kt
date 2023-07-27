cf434610f4392e7cb3ad02d75a38bc1136110a59
CharlesMuvaka <muvakacharles@gmail.com>
2023-07-27 17:34:58 +0300

    ft: define a method that splits a sentence and returns the longest word in the sentence.

--- src/main/kotlin/interview_prep/ArrayRotation.kt
+++ src/main/kotlin/interview_prep/ArrayRotation.kt
@@ -19,18 +19,29 @@ package interview_prep
  */
 fun main() {
     val numbers = arrayOf(1,2,3,4,5,6,7)
-    val d = 2
+    val d = 4
     val nums = leftRotation(numbers, d)
 
     for (i in nums.indices){
-        print(nums[i])
+        //print(nums[i])
     }
+
+    //print(fibonacci(10))
+    //print(isPalindrome("eyee"))
+    print(splitString("I yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyym a good bo00000000y"))
 
 }
 
-fun leftRotation(numbers: Array<Int>, d: Int): IntArray {
+fun leftRotation(numbers: Array<Int>, steps:Int): IntArray {
     //initialising a temporary array
     val nums = IntArray(numbers.size)
+
+    // define a variable to check wether the rotation steps is greater that the array size
+    var d = steps;
+
+    if (d > numbers.size){
+        d -= numbers.size
+    }
 
     //initialising an integer to mark our current position in the temporary array
     var counter = 0
@@ -49,4 +60,60 @@ fun leftRotation(numbers: Array<Int>, d: Int): IntArray {
         counter++
     }
     return nums
+}
+//function that returns the fibonacci number
+fun fibonacci(number: Int): Int{
+    if(number == 0 || number == 1){
+        return number
+    }
+    return fibonacci(number -1) + fibonacci(number -2)
+}
+
+//function that checks whether a given number is a palindrome
+fun isPalindrome(word: String): Boolean{
+    if (word.length == 0 || word.length == 1){
+        return true
+    }
+
+    if (word.substring(0,1) == word.substring(word.length -1)){
+        return isPalindrome(word.substring(1, word.length -1))
+    }
+    return false
+}
+
+//function that splits a string a returns the longest string
+fun splitString(sentence: String): String{
+    var result = ""
+    if(!sentence.contains(" ")){
+        result = sentence
+    }
+    val splitSentence = sentence.split(" ")
+    var counter = 0
+
+    for (i in 1 until splitSentence.size){
+        var string1 = ""
+        var string2 = ""
+        for (j in 0 until splitSentence[i].length ){
+            if (splitSentence[i][j].isLetterOrDigit()){
+                string1 += splitSentence[i][j]
+            }
+        }
+        for (j in 0 until splitSentence[counter].length ){
+            if (splitSentence[counter][j].isLetterOrDigit()){
+                string2 += splitSentence[counter][j]
+            }
+        }
+        result = string2
+        println(string1)
+
+        if (result.length < string1.length){
+            result = string2
+        }
+
+        if (result.length == string1.length){
+            result = result
+        }
+        counter++
+    }
+    return result
 }
